cbdf3e0f5999d64a5a6aefeb023e2d7d66ae2380
vuvuzela92 <kostyaostrom@gmail.com>
2026-02-01 17:32:16 +0300

    Add credit_transfers_data

--- conditional_calculation/to_pay.py
+++ conditional_calculation/to_pay.py
@@ -4,21 +4,26 @@ from utils_gs import safe_open_spreadsheet, send_df_to_google
 def main():
 	# Устанавливаем соединение с базой данных
 	connection = create_connection()
-	days_count = 1  # Количество дней для расчета
+	days_count = 396  # Количество дней для расчета
 	# Создаем SQL-запрос для получения данных
-	query = f""" WITH fin_rep_to_pay AS(SELECT 
-		f.date_from,
-		f.account,
-		SUM(CASE WHEN doc_type_name = 'Продажа' THEN ppvz_for_pay ELSE 0 END) - 
-		SUM(CASE WHEN doc_type_name = 'Возврат' THEN ppvz_for_pay ELSE 0 END) - SUM(f.delivery_rub ) - SUM(f.penalty) - SUM(f.deduction) AS "Итого к оплате"
-	FROM daily_fin_reports_full f 
-	WHERE f.date_from > CURRENT_DATE-{days_count+1}
-	GROUP BY f.date_from, f.account)
-	SELECT f.date_from,
-		SUM(f."Итого к оплате") AS "Итого к оплате",
-		f.account
-	FROM fin_rep_to_pay f
-	GROUP BY f.date_from, account;"""
+	query = f""" WITH fin_rep_to_pay AS(SELECT
+                f.date_from,
+                f.account,
+                SUM(CASE WHEN doc_type_name = 'Продажа' THEN ppvz_for_pay ELSE 0 END) -
+                SUM(CASE WHEN doc_type_name = 'Возврат' THEN ppvz_for_pay ELSE 0 END) - SUM(f.delivery_rub ) - SUM(f.penalty) - SUM(f.deduction) AS "Итого к оплате",
+                SUM(CASE WHEN f.bonus_type_name ILIKE '%заём%' 
+                	OR f.bonus_type_name ILIKE '%займ%'
+                	OR f.bonus_type_name ILIKE '%кредит%'
+                	THEN f.deduction ELSE 0 END) AS credit_transfers
+        FROM daily_fin_reports_full f
+        WHERE f.date_from > CURRENT_DATE- {days_count+1}
+        GROUP BY f.date_from, f.account)
+        SELECT f.date_from,
+                SUM(f."Итого к оплате") AS "Итого к оплате",
+                SUM(f.credit_transfers) AS "Кредит",
+                f.account
+        FROM fin_rep_to_pay f
+        GROUP BY f.date_from, account;"""
 	# Получаем данные из базы данных в виде DataFrame
 	df = get_db_table(query, connection)
 	# Преобразуем столбец date_from в строковый тип
