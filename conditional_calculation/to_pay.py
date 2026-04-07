2f7f2c2b3315b0ba9c2360572ef5c35360d99db2
vuvuzela92 <kostyaostrom@gmail.com>
2026-04-07 09:11:47 +0300

    Изменил метод добавление данных в гугл таблицу на set_with_dataframe

--- conditional_calculation/to_pay.py
+++ conditional_calculation/to_pay.py
@@ -1,5 +1,7 @@
 from utils_sql import create_connection, get_db_table
-from utils_gs import safe_open_spreadsheet, send_df_to_google
+from utils_gs import safe_open_spreadsheet
+from gspread_dataframe import set_with_dataframe
+from datetime import datetime
 
 def main():
 	# Устанавливаем соединение с базой данных
@@ -16,7 +18,7 @@ def main():
                 	OR f.bonus_type_name ILIKE '%кредит%'
                 	THEN f.deduction ELSE 0 END) AS credit_transfers
         FROM daily_fin_reports_full f
-        WHERE f.date_from > CURRENT_DATE- {days_count+1}
+        WHERE f.date_from >= '2026-01-01'
         GROUP BY f.date_from, f.account)
         SELECT f.date_from,
                 SUM(f."Итого к оплате") AS "Итого к оплате",
@@ -26,14 +28,15 @@ def main():
         GROUP BY f.date_from, account;"""
 	# Получаем данные из базы данных в виде DataFrame
 	df = get_db_table(query, connection)
-	# Преобразуем столбец date_from в строковый тип
-	df['date_from'] = df['date_from'].astype(str)
+	df['updatet_at'] = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+	# # Преобразуем столбец date_from в строковый тип
+	# df['date_from'] = df['date_from'].astype(str)
 	# Открываем таблицу Google Sheets и выбираем нужный лист   
 	table = safe_open_spreadsheet("Условный расчет")
 	# Выбираем лист "ВБ_к_оплате"
 	sheet = table.worksheet("ВБ_к_оплате")
-	# 
-	send_df_to_google(df, sheet)
+	set_with_dataframe(sheet, df)
+	print(f"Датафрейм загружен в гугл таблицу")
 
 if __name__ == "__main__":
 	main()
